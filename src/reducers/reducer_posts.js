1624a43830d18c721ee197cdb5b739047813454e
Manuel Micu <manu4216@gmail.com>
2017-10-22 16:23:33 +0200

    Add posts reducer

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,15 @@
+import { FETCH_POSTS } from '../actions';
+
+export default function(state = {}, action) {
+    switch (action.type) {
+        case FETCH_POSTS:
+            console.log(action.payload.data);
+            return action.payload.data.reduce((acc, curr) => {
+                { id, ...rest } = curr;
+            	acc[id] = rest;
+            }, {});
+            break;
+        default:
+            return state;
+    }
+}
