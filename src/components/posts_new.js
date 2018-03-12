235f69792981b82303047eb5540a87467c1bb5b9
Manuel Micu <manu4216@gmail.com>
2018-03-13 00:05:31 +0100

    Add style to the error message

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,11 +3,14 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error } } = field; // destructure meta and its sub-properties
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input className="form-control" type="text" {...field.input} />
-                {field.meta.error}
+                <div className="text-help">{touched ? error : ''}</div>
             </div>
         );
     }
