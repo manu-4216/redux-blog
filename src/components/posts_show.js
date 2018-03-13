ff7f14d1e9a8dfde1205d9713d484c9a1eb77ccf
Manuel Micu <manu4216@gmail.com>
2018-03-13 23:33:23 +0100

    Add delete post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
@@ -8,6 +8,15 @@ class PostsShow extends Component {
         const { id } = this.props.match.params; // get the :id from the URL
         this.props.fetchPost(id);
     }
+
+    onDeleteClick() {
+        const { id } = this.props.match.params; // get the :id from the URL
+
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
+    }
+
     render() {
         const { post } = this.props;
 
@@ -20,6 +29,12 @@ class PostsShow extends Component {
                 <Link className="btn btn-primary" to="/">
                     Back To Index
                 </Link>
+                <button
+                    className="btn btn-danger pull-xs-right"
+                    onClick={this.onDeleteClick.bind(this)}
+                >
+                    Delete Post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -32,4 +47,4 @@ class PostsShow extends Component {
 function mapStateToProps({ posts }, ownProps) {
     return { post: posts[ownProps.match.params.id] };
 }
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
     switch (action.type) {
@@ -11,6 +11,8 @@ export default function(state = {}, action) {
         case FETCH_POST:
             const post = action.payload.data;
             return { ...state, [post.id]: post }; // Use ES6 key interpollation to add the post
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         default:
             return state;
     }
